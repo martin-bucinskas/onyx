f7876cec00356527a54eedf7462850ae26f4a0e5
Martin Bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-04-01 21:25:25 +0100

    Initial Commit

---
+++ src/test/java/uk/co/martinsdomain/onyx/OnyxApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.martinsdomain.onyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnyxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
